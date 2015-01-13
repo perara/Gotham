7135c2465172d208e475db89ee6db863dc4bb694
Paul Lilleland <paul_r_l@hotmail.com>
2015-01-13 16:13:00 +0100

    GOTHAM-1 Added autogeneration of nodes

--- GOTHAM/Gotham/Application/Model/NodeEntity.cs
+++ GOTHAM/Gotham/Application/Model/NodeEntity.cs
@@ -3,19 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace GOTHAM.Gotham.Application.Model
 {
-  public class NodeEntity
-  {
-      public virtual int id { get; set; }
-      public virtual int tier { get; set; }
-      public virtual int priority { get; set; }
-      public virtual double bandwidth { get; set; }
-     // public ProviderEntity provider { get; set; }
+    public class NodeEntity
+    {
+        public virtual int id { get; set; }
+        public virtual int tier { get; set; }
+        public virtual int priority { get; set; }
+        public virtual double bandwidth { get; set; }
+        
+        // TODO Change to location Point object
+        public virtual int geoPosX { get; set; }
+        public virtual int geoPosY { get; set; }
 
-      ISet<NodeEntity> Neighbors { get; set; }
+        public virtual ISet<NodeEntity> siblings { get; set; }
+        public virtual string name { get; set; }
 
-      public virtual String name { get; set; }
-  }
+        public NodeEntity()
+        {
+            siblings = new ISet<NodeEntity>();
+        }
+    }
 }
--- GOTHAM/Program.cs
+++ GOTHAM/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net.Cache;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 using GOTHAM.Gotham.Application;
 using NHibernate;
 using GOTHAM.Gotham.Application.Model;
@@ -12,31 +13,66 @@ namespace GOTHAM
 {
     class Program
     {
+
+        static Random rnd = new Random();
+
         static void Main(string[] args)
         {
-
-
             // EntityManager.GetSessionFactory().Close();
             // http://www.fakenamegenerator.com/advanced.php?t=country&n%5B%5D=us&c%5B%5D=sw&gen=50&age-min=19&age-max=40
-
-            Console.WriteLine("Welcome ಠ ω ಠ");
-
-            using (var session = EntityManager.GetSessionFactory().OpenSession())
-            {
-
-
-              var nodes = session.CreateCriteria<NodeEntity>().List<NodeEntity>();
-
-            }
-            
+            Console.WriteLine("Welcome");
+            //using (var session = EntityManager.GetSessionFactory().OpenSession())
+            //{
+            //      var nodes = session.CreateCriteria<NodeEntity>().List<NodeEntity>();
+            //}
 
 
             // Wait for input
             var input = "";
+            var nodes = new List<NodeEntity>();
+            
+            for (int i = 0; i < 3; i++)
+            {
+                NodeEntity node = GenerateNode(2);
+
+                for (int j = 0; j < 10; j++)
+                {
+                    var childNode = GenerateNode(3);
+                    node.siblings.Add(childNode);
+                }
+                
+                nodes.Add(node);
+
+            }
+            
+            foreach(var node in nodes)
+            {
+                Console.WriteLine(node.geoPosX + " - " + node.geoPosY + " - Pri: " + node.priority);
+            }
+
             while ((input = Console.ReadLine()) != "e")
             {
+
                 Console.WriteLine(Globals.GetInstance().GetID());
             }
+            
+
+        }
+
+
+        public static NodeEntity GenerateNode(int tier)
+        {
+            
+            NodeEntity node = new NodeEntity();
+
+            node.geoPosX = rnd.Next(Globals.GetInstance().mapMax.X);
+            node.geoPosY = rnd.Next(Globals.GetInstance().mapMax.Y);
+            node.name = "Flette";
+            node.priority = rnd.Next(10);
+            node.tier = tier;
+            node.siblings = new ISet<NodeEntity>();
+
+            return node;
         }
     }
 }
