086c830411e9525f4a80377bd469713515b69c7a
Paul Lilleland <paul_r_l@hotmail.com>
2015-01-15 16:45:48 +0100

    GOTHAM-7 Fixed bug, Added bandwidth check to limit child nodes

--- GOTHAM/Gotham/Application/Tools/NodeGenerator.cs
+++ GOTHAM/Gotham/Application/Tools/NodeGenerator.cs
@@ -7,39 +7,74 @@ using GOTHAM.Gotham.Application.Model;
 
 namespace GOTHAM.Gotham.Application.Tools
 {
-    class NodeGenerator
+    public class NodeGenerator
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         static Random rnd = new Random();
+        
 
         public void GenerateNodes(int tier2, int tier3)
         {
             var nodes = new List<NodeEntity>();
+            //var cables = new List<CableEntity>();
 
+            // Generate Tier 2 Nodes
             for (int i = 0; i < tier2; i++)
             {
+
+                // Create a tier 2 Node
                 NodeEntity node = NewNode(2);
-
-                for (int j = 0; j < tier3; j++)
-                {
-                    var childNode = NewNode(3);
-                    var cable = NewCable(node, childNode, 100);
-                    node.siblings.Add(childNode);
-                }
-
+                node.cables = new List<CableEntity>();
+                node.bandwidth = 100000;
                 nodes.Add(node);
-            }
 
+                int bwCounter = 0;
+
+                // Check if total child bandwidth exeedes parent bandwidth
+                while (bwCounter < node.bandwidth)
+                {
+                    // Make bandwidth cap for child and add to BW counter
+                    var bwCap = rnd.Next((int)(node.bandwidth * 0.2));
+                    bwCounter += bwCap;
+
+                    // Create new Tier 3 Node
+                    var childNode = NewNode(3);
+                    childNode.cables = new List<CableEntity>();
+
+                    // Add ChildNode to parentNode
+                    node.siblings.Add(childNode);
+
+                    // Add a new Cable Beetwen nodes
+                    var cable = NewCable(node, childNode, bwCap);
+                    //cables.Add(cable);
+
+                } // Tier 3 End
+            } // Tier 2 End
+
+
+            // Iterate through tier 2 nodes
             foreach (var node in nodes)
             {
-                Console.WriteLine(node.geoPosX + " - " + node.geoPosY + " - Pri: " + node.priority);
+                var nodeBandwidth = node.bandwidth;
+
+                log.Info("\n");
+                log.Info(node.geoPosX + " - " + node.geoPosY + " - Pri: " + node.priority + " Bandwidth: " + node.bandwidth);
+
+
+                // Iterate through each of the Tier 3 siblings
                 foreach (var sibling in node.siblings)
                 {
-                    Console.WriteLine("\t" + sibling.geoPosX + " - " + sibling.geoPosY + " - Pri: " + sibling.priority + " Cable Cap: " );
+                    var siblBandwidth = node.cables.FirstOrDefault(x => x.node2 == sibling).bandwidth;
+                    log.Info("\t" + sibling.geoPosX + " - " + sibling.geoPosY + " - Pri: " + sibling.priority + " Cable Cap: " + siblBandwidth);
+                    nodeBandwidth -= siblBandwidth;
                 }
+                log.Info("Bandwidth remaining: " + nodeBandwidth);
             }
         }
 
-
+        // Make new node
+        // TODO Get relevant data
         private static NodeEntity NewNode(int tier)
         {
             var node = new NodeEntity();
@@ -54,14 +89,22 @@ namespace GOTHAM.Gotham.Application.Tools
             return node;
         }
 
+        // Make new cables
+        // TODO Get relevant data
         private static CableEntity NewCable(NodeEntity node1, NodeEntity node2, int bandwidth)
         {
             var cable = new CableEntity();
 
+            cable.node1 = node1;
+            cable.node2 = node2;
             cable.bandwidth = bandwidth;
             //cable.priority = 1;
             cable.quality = 2;
             cable.type = 4;
+
+            // Refference this cable in each node
+            node1.cables.Add(cable);
+            node2.cables.Add(cable);
 
             return cable;
         }
--- GOTHAM/Program.cs
+++ GOTHAM/Program.cs
@@ -2,6 +2,7 @@
 using GOTHAM.Gotham.Application.Tools;
 using GOTHAM.Gotham.Application;
 using GOTHAM.Gotham.Application.Model;
+using System.Collections.Generic;
 
 namespace GOTHAM
 {
@@ -15,12 +16,13 @@ namespace GOTHAM
             // EntityManager.GetSessionFactory().Close();
             // http://www.fakenamegenerator.com/advanced.php?t=country&n%5B%5D=us&c%5B%5D=sw&gen=50&age-min=19&age-max=40
             Console.WriteLine("Welcome");
-            using (var session = EntityManager.GetSessionFactory().OpenSession())
-            {
-                  var nodses = session.CreateCriteria<NodeEntity>()
-                      .List<NodeEntity>();
 
-            }
+            //using (var session = EntityManager.GetSessionFactory().OpenSession())
+            //{
+            //      var nodses = session.CreateCriteria<NodeEntity>()
+            //          .List<NodeEntity>();
+
+            //}
 
             var nodes = new NodeGenerator();
             nodes.GenerateNodes(3, 10);
